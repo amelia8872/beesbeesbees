23b6436e6dd3b61766e21c9cc7dff7a19fb5a72c
Amelia LI <jkramelia8872@gmail.com>
2023-07-10 11:19:00 -0400

    complete HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
 var HoneyMakerBee = function() {
+  // Call the bee superclass
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+// Set the prototype of HoneyMakerBee to an instance of Bee
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+// Set the constructor of HoneyMakerBee to HoneyMakerBee
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot -= 1;
+};
