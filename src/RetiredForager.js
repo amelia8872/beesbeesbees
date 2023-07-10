fecc7381142ef9303a93184af80ec9eb10c81f24
Amelia LI <jkramelia8872@gmail.com>
2023-07-10 11:20:15 -0400

    complete RetiredForager.js

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,27 @@
 var RetiredForagerBee = function() {
+  // Call the ForagerBee superclass
+  ForagerBee.call(this);
+
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+
+
 };
+
+// Set the prototype of RetiredForagerBee to an instance of ForagerBee
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+// Set the constructor of RetiredForagerBee to RetiredForagerBee
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
+
 
