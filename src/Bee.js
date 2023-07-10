cb2d342934fcef41256e23ea837e500e0a1ca761
Amelia LI <jkramelia8872@gmail.com>
2023-07-10 11:18:39 -0400

    complete Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+  // Call the Grub superclass constructor
+  Grub.call(this);
+
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+// Set the prototype of Bee to an instance of Grub
+Bee.prototype = Object.create(Grub.prototype);
+
+// Set the constructor property of Bee to Bee
+Bee.prototype.constructor = Bee;
+
 
