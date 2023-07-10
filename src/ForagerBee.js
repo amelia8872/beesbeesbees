ebf326c2bdf339ebcf03ca1ea24b45f7c5152a6b
Amelia LI <jkramelia8872@gmail.com>
2023-07-10 11:19:31 -0400

    complete ForagerBee.js

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,21 @@
 var ForagerBee = function() {
+  // Call the Bee superclass
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
+};
+
+// Set the prototype of ForagerBee to an instance of Bee
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+// Set the constructor of ForagerBee to ForagerBee
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function (treasure) {
+  this.treasureChest.push(treasure);
 };
 
